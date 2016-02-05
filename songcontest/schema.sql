3264c9a55b91900bdaf548e78af6a29853d09cdb
dp118m <dp@altruix.co>
2016-02-05 07:08:36 +0400

    Initial commit.

---
+++ songcontest/schema.sql
@@ -0,0 +1,24 @@
+SET client_min_messages TO ERROR;
+DROP SCHEMA IF EXISTS songcontest CASCADE;
+BEGIN;
+
+CREATE SCHEMA songcontest;
+SET search_path = songcontest;
+
+-- Insert table creation statements here
+
+COMMIT;
+
+----------------------------
+----------------- FUNCTIONS:
+----------------------------
+
+
+----------------------------
+------------------ TRIGGERS:
+----------------------------
+
+
+----------------------------------------
+------------------------- API FUNCTIONS:
+----------------------------------------
