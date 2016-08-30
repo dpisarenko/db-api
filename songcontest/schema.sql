25d3e31c7402de3384e77f1f5e938442761ae96b
dp118m <dp@altruix.co>
2016-08-30 22:35:04 +0400

    Added songs table DDL.

--- songcontest/schema.sql
+++ songcontest/schema.sql
@@ -1,4 +1,4 @@
-SET client_min_messages TO ERROR;
+﻿SET client_min_messages TO ERROR;
 DROP SCHEMA IF EXISTS songcontest CASCADE;
 BEGIN;
 
@@ -6,6 +6,12 @@ CREATE SCHEMA songcontest;
 SET search_path = songcontest;
 
 -- Insert table creation statements here
+
+CREATE TABLE songcontest.songs(
+	id serial primary key,
+	owner_id integer NOT NULL UNIQUE REFERENCES peeps.people(id) ON DELETE RESTRICT  	
+);
+
 
 INSERT INTO peeps.atkeys(atkey, description) VALUES('fan', 'Person is a fan.');
 INSERT INTO peeps.atkeys(atkey, description) VALUES('musician', 'Person is a musician.');
