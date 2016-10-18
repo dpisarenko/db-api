069ce3fd5ea9df51e83f38d3b27d5804ba548d89
Dmitri Pisarenko <dp@altruix.co>
2016-10-18 13:39:27 +0300

    Issue #15

--- songcontest/schema.sql
+++ songcontest/schema.sql
@@ -25,9 +25,27 @@ INSERT INTO peeps.atkeys(atkey, description) VALUES('musician', 'Person is a mus
 -- person_id - ID of the user, who uploaded the song
 -- Return value: ID of the song (which we may use as filename component).
 
-CREATE OR REPLACE FUNCTION songcontest.song_create(person_id integer) RETURNS SETOF integer AS $$
+CREATE OR REPLACE FUNCTION songcontest.song_create(person_id integer, 
+	OUT status smallint) RETURNS AS $$
+DECLARE
+	err_code text;
+	err_msg text;
+	err_detail text;
+	err_context text;
 BEGIN
-	RETURN QUERY INSERT INTO songcontest.songs(owner_id) VALUES(person_id) RETURNING songcontest.songs.id;
+	status := 200;
+	js := row_to_json(r) FROM (QUERY INSERT INTO songcontest.songs(owner_id) VALUES(person_id) RETURNING songcontest.songs.id) r;
+EXCEPTION
+	WHEN OTHERS THEN GET STACKED DIAGNOSTICS
+		err_code = RETURNED_SQLSTATE,
+		err_msg = MESSAGE_TEXT,
+		err_detail = PG_EXCEPTION_DETAIL,
+		err_context = PG_EXCEPTION_CONTEXT;
+	status := 500;
+	js := json_build_object(
+		'type', 'http://www.postgresql.org/docs/9.4/static/errcodes-appendix.html#' || err_code,
+		'title', err_msg,
+		'detail', err_detail || err_context);
 END;
 $$ LANGUAGE plpgsql;
 
