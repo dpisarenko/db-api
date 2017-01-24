f4757727d20ef2714c1d4622773936a87292878d
dp118m <dp@altruix.co>
2017-01-24 09:29:23 +0300

    Issue #29.

---
+++ songcontest/issue29.sql
@@ -0,0 +1,7 @@
+-- Script for modifying the database in scope of issue 29
+
+TODO: Create table contestStatuses
+TODO: Fill table contestStatuses
+TODO: Create table contests
+TODO: Add user type "organizer"
+TODO: modify the table feedback so that it contains a link to the contestStatuses database.
