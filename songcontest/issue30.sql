f70bee6910d0e8dd354cdc52817c860a9ebcc388
dp118m <dp@altruix.co>
2017-01-25 08:34:14 +0300

    Issue 30: Added table definition contests.

--- songcontest/issue30.sql
+++ songcontest/issue30.sql
@@ -1,7 +1,5 @@
 -- Script for modifying the database in scope of issue 29
 
-TODO: Create table contestStatuses
-
 CREATE TABLE songcontest.contestStatuses(
 	id integer primary key,
 	name VARCHAR(256)
@@ -16,6 +14,12 @@ INSERT INTO songcontest.contestStatuses(id, name) VALUES(5, "Finishing");
 INSERT INTO songcontest.contestStatuses(id, name) VALUES(6, "Completed");
 INSERT INTO songcontest.contestStatuses(id, name) VALUES(7, "Canceled");
 
-TODO: Create table contests
+CREATE TABLE songcontest.contests(
+	id serial primary key,
+	name VARCHAR(256),
+	sponsor_description TEXT,
+	state integer NOT NULL REFERENCES songcontest.statuses(id) ON DELETE RESTRICT,
+	terms_description TEXT
+);
 TODO: Add user type "organizer"
 TODO: modify the table feedback so that it contains a link to the contestStatuses database.
